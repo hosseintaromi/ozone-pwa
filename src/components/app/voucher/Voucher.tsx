e5763c98b901d79ba297f637155559e2340cc0e4
xerxes <mohammadsolgi96@gmail.com>
2024-08-12 17:23:07 +0330

    feat: add coupon empty list

--- src/components/app/voucher/Voucher.tsx
+++ src/components/app/voucher/Voucher.tsx
@@ -4,12 +4,13 @@ import Header from '@/components/app/voucher/components/Header';
 import TabGroup, { TabPanel, TabPanels, Tabs } from '@/components/@base/tab';
 import { TabDefault } from '@/components/@base/tab/stylish';
 import locale from '@/locale';
+import XImage from '@/components/share/x-image';
 
 const Voucher = () => {
   const {
     common: { usable, used, expired },
     app: {
-      voucher: { couponCanDisable },
+      voucher: { couponCanDisable, couponUnavailable },
     },
   } = locale;
   return (
@@ -27,7 +28,19 @@ const Voucher = () => {
               {couponCanDisable}
             </Text>
           </TabPanel>
-          <TabPanel>2</TabPanel>
+          <TabPanel>
+            <Container center className='h-[calc(100dvh-220px)] flex-col gap-3'>
+              <XImage
+                src='/images/image/emptyStates/emptyList.svg'
+                alt='empty list'
+                width={70}
+                height={70}
+              />
+              <Text size={SIZE_ENUM.SM} bold className='text-neutral-500'>
+                {couponUnavailable}
+              </Text>
+            </Container>
+          </TabPanel>
           <TabPanel>3</TabPanel>
         </TabPanels>
       </TabGroup>
